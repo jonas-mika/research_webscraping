c3467ceac8faf10a94d5d8e0724b461be379633a
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2020-11-01 13:47:04 +0100

    Add Formatting to Execution

--- twitter/twitter_scraper.py
+++ twitter/twitter_scraper.py
@@ -2,7 +2,7 @@
 import json
 from random import choice
 from classes import TwitterScraper 
-
+from timeit import default_timer as timer
 
 # load configurations
 with open('config.json', 'r') as f:
@@ -16,16 +16,25 @@ pw = CONFIG['CREDENTIALS']['PASSWORD']
 
 # executed script
 def main():
+    print('Initializing Twitter Scraper with given credentials...')
     twitter_scraper = TwitterScraper(un, pw, path)
     twitter_scraper.login()
-    print(twitter_scraper.username)
-    print(twitter_scraper.get_data())
+    print(f'Logged in as:   {twitter_scraper.username}')
+    print('-------------------')
+    print('\nStarting to scrape...')
 
     for searchterm in searchterms:
+        print(f'\nStarting to scrape [{searchterm}]')
+        start = timer()
         twitter_scraper.scrape(searchterm)
-        print(f'Finished Scraping for {searchterm}')      
-    print(len(twitter_scraper.get_data()))
+        stop = timer()
+        print(f'Finished Scrape in {round(stop-start, 2)}')
+        print('-------------------')
+        break
+    print(f'Scraped {len(twitter_scraper.get_data())} Searchterms')
+    print(f'Saving data...')
     twitter_scraper.save_data()
     twitter_scraper.close_driver()
+    print('DONE!')
 
 main()
